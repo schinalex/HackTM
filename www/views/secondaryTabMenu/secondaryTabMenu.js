61e76a472feeb37a60fe6557dd430a24a4a8d0f3
DenisTDR <denis.tdr@gmail.com>
2016-05-21 22:03:59 +0300

    controller for secondaryTabMenu, draggable map

---
+++ www/views/secondaryTabMenu/secondaryTabMenu.js
@@ -0,0 +1,10 @@
+/**
+ * Created by NM on 5/21/2016.
+ */
+
+appControllers
+  .controller('SecondaryMenuController', function($scope) {
+    console.log("secondary tabs menu controller loaded");
+
+  });
+
