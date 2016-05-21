e2463fa9767a3f8656a577fe36ca891d93955154
DenisTDR <denis.tdr@gmail.com>
2016-05-21 05:42:25 +0300

    fixed maps and added some hardcoded markers for tests

--- www/views/map/map.js
+++ www/views/map/map.js
@@ -6,40 +6,31 @@ console.log("map controller loading");
 
 appControllers
 
-  .controller('MapController', function($scope, uiGmapGoogleApi) {
+  .controller('MapController', function($scope, uiGmapGoogleMapApi) {
     console.log("map controller loaded");
-    $scope.settings = {
-      enableFriends: true
-    };
+
+    $scope.map = { center: { latitude: 45.745139, longitude: 21.241582 }, zoom: 16 };
+
+    $scope.randomMarkers = [];
 
     uiGmapGoogleMapApi.then(function(maps){
-      var initMap = function () {
-        var stations = [
-          {
-            station: 'Sala Olimpia',
-            coords: {lat: 45.745139, lng: 21.241582}
-          },
-          {
-            station: 'Deliblata',
-            coords: {lat: 45.747164, lng: 21.246754}
-          }
-        ]
-
-        var myLatLng = {lat: 45.745139, lng: 21.241582}
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: myLatLng,
-          scrollwheel: false,
-          zoom: 16
-        })
-
-        for (let station of stations) {
-          new google.maps.Marker({
-            map: map,
-            position: station.coords,
-            title: station.station
-          })
+      console.log("loaded maps");
+      var markers = [
+        {
+          latitude: "45.745139",
+          longitude: "21.241582",
+          title: "first",
+          id: 1
+        },
+        {
+          latitude: "45.747164",
+          longitude: "21.246754",
+          title: "second",
+          id: 2
         }
+      ];
+      for(let marker of markers){
+        $scope.randomMarkers.push(marker);
       }
-    })
+    });
   });
