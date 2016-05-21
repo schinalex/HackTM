cc2907dcc42188dc30bc4e9df021c5f677526e9b
Alex <schinalex@gmail.com>
2016-05-21 04:31:37 +0300

    adding google maps

--- www/views/map/map.js
+++ www/views/map/map.js
@@ -6,9 +6,39 @@ console.log("map controller loading");
 
 appControllers
 
-  .controller('MapController', function($scope) {
+  .controller('MapController', function($scope, uiGmapGoogleApi) {
     console.log("map controller loaded");
     $scope.settings = {
       enableFriends: true
     };
+    uiGmapGoogleMapApi.the(function(maps){
+      var initMap = function () {
+        var stations = [
+          {
+            station: 'Sala Olimpia',
+            coords: {lat: 45.745139, lng: 21.241582}
+          },
+          {
+            station: 'Deliblata',
+            coords: {lat: 45.747164, lng: 21.246754}
+          }
+        ]
+
+        var myLatLng = {lat: 45.745139, lng: 21.241582}
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+          zoom: 16
+        })
+
+        for (let station of stations) {
+          new google.maps.Marker({
+            map: map,
+            position: station.coords,
+            title: station.station
+          })
+        }
+      }
+    })
   });
